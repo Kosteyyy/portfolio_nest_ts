07135e0030e61b6e13320673f73de35103fa2ae5
Kosteyyy <averkiev@yandex.ru>
2022-11-02 17:30:59 +0300

    contact page phone and email

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -1,6 +1,7 @@
 import React, { FormEvent, useEffect } from "react";
 import styles from "./index.module.scss";
 // import { sendMessage } from "../utils/sendMessage";
+import { FaPhone, FaRegEnvelope } from "react-icons/fa";
 import { useForm } from "../../customHooks/useForm";
 import { TFormData } from "../../types";
 import { messageFromObject } from "../../utils/messageFromObject";
@@ -63,40 +64,62 @@ const Contact = () => {
       <div className="container">
         <h2>Контакты</h2>
         <h3>Будем на связи</h3>
-        <div className={styles.formContainer}>
-          <form
-            className={styles.form}
-            onChange={handleInputChange}
-            onSubmit={handleSubmit}
-          >
-            <div className={styles.inputBox}>
-              <input type="text" name="name" /*  value={name}  */ required />
-              <span>Имя</span>
-              <i></i>
-            </div>
-            <div className={styles.inputBox}>
-              <input type="text" name="phone"/*  value={phone} */ required />
-              <span>Телефон или E-mail</span>
-              <i></i>
-            </div>
-            {/* <div className={styles.inputBox}>
+        <div className={styles.contactBlock}>
+          <div className={styles.contactContainer}>
+            <h2>Я открыт к сотрудничеству</h2>
+            <p>
+              Вы можете связаться со мной по телефону, электронной почте или
+              отправить сообщение через форму, я получу его на телефон.
+            </p>
+            <p>
+              <FaPhone /> <span className={styles.phone}>+7-916-341-90-77</span>
+            </p>{" "}
+            <p>
+              <FaRegEnvelope />{" "}
+              <span className={styles.email}>
+                <a href="mailto: 7456052@gmail.com">7456052@gmail.com</a>
+              </span>
+            </p>
+          </div>
+          <div className={styles.formContainer}>
+            <form
+              className={styles.form}
+              onChange={handleInputChange}
+              onSubmit={handleSubmit}
+            >
+              <div className={styles.inputBox}>
+                <input type="text" name="name" /*  value={name}  */ required />
+                <span>Имя</span>
+                <i></i>
+              </div>
+              <div className={styles.inputBox}>
+                <input type="text" name="phone" /*  value={phone} */ required />
+                <span>Телефон или E-mail</span>
+                <i></i>
+              </div>
+              {/* <div className={styles.inputBox}>
               <input type="text" required />
               <span>E-mail</span>
               <i></i>
             </div> */}
-            <div className={styles.inputBox}>
-              <input type="text" name="subject"/*  value={subject}  */required />
-              <span>Тема</span>
-              <i></i>
-            </div>
-            <div className={styles.textBox}>
-              <textarea required /* value={message} */ name="message" />
-              <span>Сообщение</span>
-              <i></i>
-            </div>
-            <button>Отправить сообщение</button>
-          </form>
-        </div>{" "}
+              <div className={styles.inputBox}>
+                <input
+                  type="text"
+                  name="subject"
+                  /*  value={subject}  */ required
+                />
+                <span>Тема</span>
+                <i></i>
+              </div>
+              <div className={styles.textBox}>
+                <textarea required /* value={message} */ name="message" />
+                <span>Сообщение</span>
+                <i></i>
+              </div>
+              <button>Отправить сообщение</button>
+            </form>
+          </div>{" "}
+        </div>
       </div>
     </section>
   );
